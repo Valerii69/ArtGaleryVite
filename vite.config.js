5992016df6398da893564c22e04464dbe141ced3
Valerii Viun <2455521@gmail.com>
2025-02-24 19:36:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://Valerii69.github.io/ArtGalery-vite/",
   plugins: [react()],
 })
