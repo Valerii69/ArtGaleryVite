9766eebe2603fbe2dad9f044f800bf0f58758f7d
Valerii69 <2455521@gmail.com>
2025-02-25 16:28:25 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import {ArtGallery} from "../src/ArtGallery/ArtGallery"
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
   
         <ArtGallery/>
- 
 
 
   )
