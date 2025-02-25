a57dbe6c456f683e811b70f7f756994856e9a263
Valerii69 <2455521@gmail.com>
2025-02-25 16:07:28 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"https://Valerii69.github.io/ArtGalery-vite/",
+  base:"https://Valerii69.github.io/ArtGaleryVite/",
   plugins: [react()],
 })
