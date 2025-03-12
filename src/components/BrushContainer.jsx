9a0d8462ae32d7b77c423ad351eea47c353a64b3
Valerii69 <2455521@gmail.com>
2025-03-12 14:41:01 +0200

    big fix BrushContainer

--- src/components/BrushContainer.jsx
+++ src/components/BrushContainer.jsx
@@ -1,9 +1,43 @@
+import React from "react";
+import Typed from "typed.js";
 import "../components/BrushContainer/brushContainer.css";
+import "../components/IntroBrush/intro.css";
+import "../components/Flower/flower.css";
 
 export default function BrushContainer() {
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      // strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+      strings: [
+        "А жінка буває на осінь так схожа То тиха й привітна, а то непогожа, То скропить сльозою, то сонцем засвітить, То прагне зими, то вертається в літо.",
+      ],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <div className="brush-container">
-      <p>BrushContainer</p>
+    <div className="card">
+      {/* <div> стовпчик 1</div> */}
+      <div className="content-container">
+        <div className="flower-cont with-fill"> стовпчик 1</div>
+        <div className="intro-cont">
+          <span ref={el} />
+        </div>
+      </div>
     </div>
   );
 }
+
+// export default function BrushContainer() {
+//   return (
+
+//   );
+// }
---
+++ src/components/IntroBrush/Intro.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import "./intro.css";
+
+import { Flower } from "../Flower/Flower";
+import { Title } from "../Title/Title";
+
+export const Intro = (props) => {
+  const [displayRestart, setDisplayRestart] = useState(false);
+
+  console.log(props.animationPlayed);
+  return (
+    <div className="intro-cont">
+      {props.animationPlayed && (
+        <button
+          className="restart-anim-cont"
+          onClick={() => {
+            props.setAnimationPlayed(false);
+            setDisplayRestart(!displayRestart);
+          }}
+        >
+          <i className="fas fa-redo restart-anim-icon"></i>
+        </button>
+      )}
+
+      <Flower
+        setAnimationPlayed={props.setAnimationPlayed}
+        animationPlayed={props.animationPlayed}
+        displayRestart={displayRestart}
+      />
+      <Title
+        setAnimationPlayed={props.setAnimationPlayed}
+        animationPlayed={props.animationPlayed}
+      />
+    </div>
+  );
+};
