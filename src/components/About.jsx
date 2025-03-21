b2890925258a4020cba07cfa2aa804d8b7b1a7ce
Valerii69 <2455521@gmail.com>
2025-03-21 15:49:37 +0200

    summary fixed style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,8 @@
-// import React from "react";
 import { Link } from "react-router-dom";
-// import { useState } from "react";
-// import Typed from "typed.js";
-// import { Intro } from "../components/IntroBrush/Intro";
+import { useState, useEffect } from "react";
+ import { AboutCard } from "../components/AboutCard/AboutCard";
 import Angel from "../public/icons/AngleDuble.svg";
-// import Logo from "../public/logo2.png";
+import alla from "../public/Images/алла3.png";
 
 import "../components/BrushContainer/brushContainer.css";
 import "../components/IntroBrush/intro.css";
@@ -13,21 +11,41 @@ import "../components/Flower/flower.css";
 import "../../src/components/ArtGallery/art-gallery.css";
 
 export default function About() {
- return (
-    <div className="card">
-      <Link to="/">
-        <button className="return-anim-cont">
-          <img
+  const [imgSize, setImgSize] = useState(240); // Початковий розмір
+
+  useEffect(() => {
+    function getImgSize() {
+      if (window.innerWidth < 480) return 140;
+      if (window.innerWidth < 768) return 150;
+      else return 240;
+    }
+
+    function handleResize() {
+      setImgSize(getImgSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Немає залежностей, але resize працюватиме
+
+  return (
+    <div className="aboutContainer">
+      <AboutCard></AboutCard>
+      <div className="cardAbout">
+        <Link to="/">
+          <button className="return-anim-cont">
+            <img
               src={Angel}
-            alt="Angel Icon"
-            width="24"
-            height="24"
-            className="return-icon"
-          />
-          <i className="return-anim-icon"></i>
-        </button>
-      </Link>
-      {/* <button
+              alt="Angel Icon"
+              width="24"
+              height="24"
+              className="return-icon"
+            />
+            <i className="return-anim-icon"></i>
+          </button>
+        </Link>
+
+        {/* <button
         className="restart-anim-cont"
         onClick={() => {
           setAnimationPlayed(false);
@@ -44,8 +62,18 @@ export default function About() {
         />
         <i className="restart-anim-icon"></i>
       </button> */}
-      <div className="content-container">
-   щось треба придумати
+        <div className="content-container">
+          <img
+            id="brushImg"
+            className="brush-about"
+            src={alla}
+            alt="alla"
+            width={imgSize}
+            height="auto"
+          />
+
+          {/* <AboutCard tab={tab} openReadMore={openReadMore}></AboutCard> */}
+        </div>
       </div>
     </div>
   );
